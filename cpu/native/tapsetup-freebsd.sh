226d285a6d223fa3297ef33c0f93ee9f69a2e14e
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2013-10-04 09:57:38 +0200

    improve (untested) tapsetup-freebsd.sh

--- cpu/native/tapsetup-freebsd.sh
+++ cpu/native/tapsetup-freebsd.sh
@@ -1,8 +1,54 @@
 #!/bin/sh
 
-ifconfig bridge0 create
-echo "start riot instances"
-ifconfig bridge0 up
-ifconfig tap0 up
-ifconfig tap1 up
-ifconfig bridge0 addm tap0 addm tap1
+COMMAND=${1}
+COUNT=${2}
+
+DEFCOUNT="2"
+
+if [ -z "${USER}" ]; then
+    echo 'need to export $USER'
+    exit 1
+fi
+if [ -z "${COMMAND}" ]; then
+    echo "usage: $(basename $0) <create [count]|delete>"
+    exit 1
+fi
+
+if [ "${COMMAND}" = 'create' ]; then
+    if [ -z "${COUNT}" ]; then
+        COUNT="${DEFCOUNT}"
+    fi
+
+    # load kernel modules
+    sudo kldload if_tap
+    sudo kldload if_bridge
+
+    # set permissions
+    sudo sysctl net.link.tap.user_open=1
+    sudo chown ${USER} /dev/tap0
+
+    # create network
+    echo "creating ${BRNAME} ..."
+    ifconfig bridge0 create
+    ifconfig bridge0 up
+
+    for N in $(seq 0 "$((COUNT - 1))"); do
+        echo "start riot for tap${N} ..."
+        ifconfig tap${N} up
+        ifconfig bridge0 addm tap${N}
+    done
+
+elif [ "${COMMAND}" = 'delete' ]; then
+    # unload kernel modules
+    sudo kldunload if_tap
+    sudo kldunload if_bridge
+
+    # reset permissions (devices already destroyed)
+    sudo sysctl net.link.tap.user_open=0
+
+else
+    echo 'unknown command'
+    exit 1
+fi
+
+exit 0
