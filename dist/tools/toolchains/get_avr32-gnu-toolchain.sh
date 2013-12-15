99d3682fd4a1194d9258bb44674733a721011d6b
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2013-12-15 13:47:35 +0100

    initial import

---
+++ dist/tools/toolchains/get_avr32-gnu-toolchain.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+URL_LINUX_32=http://www.atmel.com/Images/avr32-gnu-toolchain-3.4.2.435-linux.any.x86.tar.gz
+URL_LINUX_64=http://www.atmel.com/Images/avr32-gnu-toolchain-3.4.2.435-linux.any.x86_64.tar.gz
+
+# This contains directions for building the toolchain from source:
+# http://simplemachines.it/downloads/Mizar32/docs/Mizar32%20Quick%20Start%20Guide%2020130916.pdf
